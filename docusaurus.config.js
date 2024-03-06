83c76e320d46369544a0bf7b87e2b422afc41069
Pavel Duchovny <pash10g@gmail.com>
2024-03-06 10:26:24 +0200

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
   title: `${title}`,
   tagline: `${tagLine}`,
   url: `https://${workshopName}.github.io`,
-  baseUrl: `/${workshopName}/`,
+  baseUrl: `/`,
   projectName: `${organizationName}.github.io`,
   organizationName: `${organizationName}`,
   trailingSlash: false,
